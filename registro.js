7cc05965e4dc1c6ec098b595580b71f79eeed699
alujang25-cmyk <alujang2543@gmail.com>
2026-09-03 15:37:40 -0500

    Creacion de registro

---
+++ registro.js
@@ -0,0 +1,4 @@
+// Función para registrar nuevo estudiante
+function registrarEstudiante(nombre, codigo, carrera) {
+    console.log(`Estudiante ${nombre} con código ${codigo} registrado en ${carrera}.`);
+}
