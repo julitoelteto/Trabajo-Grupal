7c000be589187ba3a6c168366b11f0f38107dc75
Mleon2805 <n00409490@upn.pe>
2026-09-04 22:37:52 -0500

    Reporte General Estudiante

---
+++ Reporte.js
@@ -0,0 +1,6 @@
+// reportes.js - Versión Básica
+
+function generarReporteGeneral() {
+    console.log("--- REPORTE GENERAL DE ESTUDIANTES ---");
+    console.log("Mostrando listado general de matriculados...");
+}
