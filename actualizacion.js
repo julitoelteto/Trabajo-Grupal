10b8a189a51b04c1ddba4d76d283635e4550b744
Julio <N00249824@upn.pe>
2026-09-04 21:56:07 -0500

    Creacion de actualizacion

---
+++ actualizacion.js
@@ -0,0 +1,6 @@
+function actualizarEstudiante(codigo, nuevoNombre, nuevaCarrera) {
+    console.log(`Actualizando datos del estudiante ${codigo}...`);
+    // Simulación de actualización
+    console.log(`Nuevos datos: Nombre: ${nuevoNombre}, Carrera: ${nuevaCarrera}`);
+    return true;
+}
